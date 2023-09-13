cbd92adbae6b34b95792df47f1b781494e13d7e9
FK78 <34196381+FK78@users.noreply.github.com>
2023-09-13 17:37:15 +0100

    1.9 unicafe step4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,17 +1,20 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
-const Statistics = ({good, neutral, bad, all}) => {
-  return (
-    <div>
-      <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {good + neutral + bad}</p>
-      <p>Average {(good - bad) / all}</p>
-    </div>
-  );
+const Statistics = ({ good, neutral, bad, all }) => {
+  if (!all) {
+    return <p>No feedback given</p>;
+  } else {
+    return (
+      <div>
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+        <p>All {good + neutral + bad}</p>
+        <p>Average {(good - bad) / all}</p>
+      </div>
+    );
+  }
 };
 
 const App = () => {
@@ -25,6 +28,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <h1>Statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} all={all} />
     </div>
   );
