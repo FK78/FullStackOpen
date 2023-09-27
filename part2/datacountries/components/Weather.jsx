3dfa0c2b75c0390b4c5a100329b67b62e00e497e
FK78 <34196381+FK78@users.noreply.github.com>
2023-09-27 20:32:43 +0100

    clean up weatherURL

--- part2/datacountries/components/Weather.jsx
+++ part2/datacountries/components/Weather.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/prop-types */
 
 const Weather = ({ capital, weatherData }) => {
-  const weatherURL = "https://openweathermap.org/img/wn/";
+  const weatherURL = `https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
 
   return (
     <div>
       <h2>Weather in {capital}</h2>
       <p>Temperature {weatherData.temp}°C</p>
-      <img src={`${weatherURL}${weatherData.icon}@2x.png`} alt="weather icon" />
+      <img src={weatherURL} alt="weather icon" />
       <p>Wind speed {weatherData.windSpeed} m/s</p>
     </div>
   );
