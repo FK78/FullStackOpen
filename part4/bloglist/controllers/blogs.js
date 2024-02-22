b7d733154410f680eb4d87bd794c311f2c76c0e1
FK78 <34196381+FK78@users.noreply.github.com>
2024-02-22 18:48:40 +0000

    4.12*: Blog List tests, step 5

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,23 +1,27 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
 
-blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
-    response.json(blogs)
-})
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
 
-blogsRouter.post('/', async (request, response) => {
-    const body = request.body
+blogsRouter.post("/", async (request, response) => {
+  const body = request.body;
 
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes ? body.likes : 0
-    })
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes ? body.likes : 0,
+  });
 
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
-})
+  if (blog.title === undefined || blog.url === undefined) {
+    response.status(400).json();
+  } else {
+    const savedBlog = await blog.save();
+    response.status(201).json(savedBlog);
+  }
+});
 
-module.exports = blogsRouter
+module.exports = blogsRouter;
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -62,6 +62,44 @@ test('likes property defaults to zero if missing', async () => {
     expect(mappedBlog[0].likes).toEqual(0)
 })
 
+test('send 400 Bad Request if title is missing', async () => {
+    const newBlogObject = {
+        _id: "5a533a451b54a676234d27f7",
+        author: "Amazon",
+        url: "https://amazon.com/",
+        likes: 50,
+        __v: 0
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlogObject)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+
+    const dataAfterPost = await helper.blogsInDB()
+    expect(dataAfterPost).toHaveLength(helper.initalBlogs.length);
+})
+
+test('send 400 Bad Request if url is missing', async () => {
+    const newBlogObject = {
+        _id: "1a533a451b54a676234d27f7",
+        author: "BookFace",
+        title: "BookFace",
+        likes: 30,
+        __v: 0
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlogObject)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+    
+    const dataAfterPost = await helper.blogsInDB()
+    expect(dataAfterPost).toHaveLength(helper.initalBlogs.length);
+})
+
 
 afterAll(async () => {
     await mongoose.connection.close()
